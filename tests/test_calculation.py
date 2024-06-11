8f6313e1d501f5c722088c884e249d5b1523fa81
saiabhishek-mgv <119317223+saiabhishek-mgv@users.noreply.github.com>
2024-06-10 22:32:42 -0400

    removed duplicate parameterization

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,8 +1,4 @@
-"""
-The tests are designed to verify the correctness of basic arithmetic operations
-(addition, subtraction, multiplication, division) implemented in the calculator.operations module,
-as well as the functionality of the Calculation class that encapsulates these operations.
-"""
+"""Tests to verify the correctness of basic arithmetic operations """
 
 #pylint: disable=unnecessary-dunder-call, invalid-name # type: ignore
 from decimal import Decimal
@@ -10,26 +6,8 @@ import pytest
 from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
-@pytest.mark.parametrize("a, b, operation, expected", [
-    (Decimal('10'), Decimal('5'), add, Decimal('15')),  # Test addition
-    (Decimal('10'), Decimal('5'), subtract, Decimal('5')),  # Test subtraction
-    (Decimal('10'), Decimal('5'), multiply, Decimal('50')),  # Test multiplication
-    (Decimal('10'), Decimal('2'), divide, Decimal('5')),  # Test division
-    (Decimal('10.5'), Decimal('0.5'), add, Decimal('11.0')),  # Test addition with decimals
-    (Decimal('10.5'), Decimal('0.5'), subtract, Decimal('10.0')),  # Test subtraction with decimals
-    (Decimal('10.5'), Decimal('2'), multiply, Decimal('21.0')),  # Test multiplication with decimals
-    (Decimal('10'), Decimal('0.5'), divide, Decimal('20')),  # Test division with decimals
-])
 def test_calculation_operations(a, b, operation, expected):
-    """
-    Test calculation operations with various scenarios.
-    
-    Parameters:
-        a (Decimal): The first operand in the calculation.
-        b (Decimal): The second operand in the calculation.
-        operation (function): The arithmetic operation to perform.
-        expected (Decimal): The expected result of the operation.
-    """
+    """Test calculation operations with various scenarios. """
     calc = Calculation(a, b, operation)
     assert calc.perform() == expected, f"Failed {operation.__name__} operation with {a} and {b}"
 
