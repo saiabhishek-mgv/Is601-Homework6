540292f46d9a496825bb8d99130f95516dff1550
saiabhishek-mgv <119317223+saiabhishek-mgv@users.noreply.github.com>
2024-06-10 21:43:44 -0400

    updated the test_operations to pass the tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,25 +5,10 @@ from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
 
-def test_operation_add():
-    '''Testing the addition operation'''
-    calculation = Calculation(Decimal('10'), Decimal('5'), add)
-    assert calculation.perform() == Decimal('15'), "Add operation failed"
-
-def test_operation_subtract():
-    '''Testing the subtract operation'''
-    calculation = Calculation(Decimal('10'), Decimal('5'), subtract)
-    assert calculation.perform() == Decimal('5'), "Subtract operation failed"
-
-def test_operation_multiply():
-    '''Testing the multiply operation'''
-    calculation = Calculation(Decimal('10'), Decimal('5'), multiply)
-    assert calculation.perform() == Decimal('50'), "Multiply operation failed"
-
-def test_operation_divide():
-    '''Testing the divide operation'''
-    calculation = Calculation(Decimal('10'), Decimal('5'), divide)
-    assert calculation.perform() == Decimal('2'), "Divide operation failed"
+def test_operation(a, b, operation, expected):
+    '''Testing various operations'''
+    calculation = Calculation.create(a, b, operation)
+    assert calculation.perform() == expected, f"{operation.__name__} operation failed"
 
 def test_divide_by_zero():
     '''Testing the divide by zero exception'''
